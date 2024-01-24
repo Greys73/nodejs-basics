10779b041a60898c70c7e74267b6e5a941da788e
Greys73 <v.myazin@gmail.com>
2024-01-24 22:37:56 +0400

    feat: fs#read, fs#create, fs#copy Wed, Jan 24, 2024 10:37:56 PM

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from 'node:url';
+
+const dirName = path.dirname(fileURLToPath(import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+  const sourceDir = path.join(dirName, 'files');
+  const targetDir = path.join(dirName, 'files_copy');
+  try {
+    const files = await fs.promises.readdir(sourceDir, { withFileTypes: true });
+    await fs.promises.mkdir(targetDir);
+    files.forEach(async (file) => {
+      const sourceFilePath = path.join(sourceDir, file.name);
+      const targetFilePath = path.join(targetDir, file.name);
+      await fs.promises.copyFile(sourceFilePath, targetFilePath);
+    });
+  } catch {
+    console.error('FS operation failed');
+  }
 };
 
 await copy();
