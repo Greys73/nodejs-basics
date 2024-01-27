548bb67d6f7b0088cd9cba8b6648220bc064f8b5
Greys73 <v.myazin@gmail.com>
2024-01-27 12:05:37 +0400

    feat: wt Sat, Jan 27, 2024 12:05:37 PM

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,22 @@
+import os from 'os';
+import wt from 'worker_threads';
+
+const INCREMENT = 10;
+const WORKER_PATH = './src/wt/worker.js';
+
 const performCalculations = async () => {
-    // Write your code here
+  const numOfCores = os.cpus().length;
+  const workersPromises = [];
+  for(let i = 0; i < numOfCores; i++) {
+    const workPromise = new Promise((resolve) => {
+      const worker = new wt.Worker(WORKER_PATH, {workerData: INCREMENT + i});
+      worker.on('message', (data) => resolve({ status: 'resolved', data }));
+      worker.on('error', () => resolve({ status: 'error', data: null }));
+    });
+    workersPromises.push(workPromise);
+  }
+  const result = await Promise.all(workersPromises);
+  console.log(result);
 };
 
 await performCalculations();
